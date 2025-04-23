2b35d179be94aab31a75068e1e95d2de2f74363b
felo <felo.safwat2000@gmail.com>
2025-04-23 07:18:58 +0200

    done

--- src/modules/message/message.validation.js
+++ src/modules/message/message.validation.js
@@ -2,6 +2,8 @@ import Joi from "joi";
 import { genralValidation } from "../../middleware/valdation.middelware.js";
 
 export const messageValidation = Joi.object().keys({
-    message:Joi.string().min(5).max(50000).required(),
-    recipientId:genralValidation.userId.required()
+    message: Joi.string().min(5).max(50000).required(),
+    recipientId: genralValidation.userId.required(),
+    "accept-language": genralValidation["accept-language"]
+
 }).required()
