13954edca45f946f5e2d22dc9d3de5c3f9f7f5ae
felo <felo.safwat2000@gmail.com>
2025-05-06 06:49:54 +0300

    Done

--- src/modules/message/message.controller.js
+++ src/modules/message/message.controller.js
@@ -3,11 +3,8 @@ import * as messageServices from "./services/message.service.js"
 import { valdation } from "../../middleware/valdation.middelware.js";
 import { messageValidation } from "./message.validation.js";
 const router = Router()
-router.post("/",
-    valdation(messageValidation),
-    messageServices.sendMassege)
-router.put("/addFavorite",
-    messageServices.addFavoriteMassege)
-router.put("/delete",
-    messageServices.deleteMassege)
+
+router.post("/",valdation(messageValidation),messageServices.sendMassege)
+router.put("/addFavorite",messageServices.toggleFavoriteMessage)
+router.put("/delete",messageServices.deleteMassege)
 export default router
